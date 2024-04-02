f1a75e920cc4702b55306e61880604cf3e73b264
hurairaFurqan <hurairafurqan13@gmail.com>
2024-04-02 16:09:53 +1100

    login feature done

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -10,7 +10,39 @@ exports.signUp = catchAsync(async (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-    console.log(user);
     const dataToSave = await user.save();
     res.status(200).json({ status: "Success", dataToSave })
+})
+
+
+exports.signIn = catchAsync(async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    if (!email && !password) {
+        res.json(400).json("email or password missing")
+    }
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+        res.status(400).json("no user found. Please register yourself")
+    }
+
+    const result = await user.correctPassword(password, user.password)
+
+    if (result) {
+        const token = jwt.sign({
+            exp: Math.floor(Date.now() / 1000) + 60 * 60,
+            iat: Math.floor(Date.now() / 1000),
+            id: user._id,
+        }, process.env.SECRET_KEY)
+
+        const data = {
+            token,
+            user
+        }
+
+        res.status(200).json(data)
+    } else {
+        res.status(404).json("wrong password")
+    }
 })
--- server/routers/auth.router.js
+++ server/routers/auth.router.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const authController = require("../controllers/auth.controllers");
 
 router.post("/signUp", authController.signUp)
+router.post("/signIn", authController.signIn)
 
 module.exports = router
